35e14bc5277eb4c40be847cc307ad926211dbccb
Daniel Shogbon <ishogbon@gmail.com>
2025-01-20 16:33:56 +0100

    added json response

--- server/server.go
+++ server/server.go
@@ -1,19 +1,37 @@
 package server
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
-type ReturnData struct {
-	message string
+type Data struct{}
+
+type Response struct {
+	Status  string
+	Message string
+	Data    Data
+}
+
+func (rd *Response) writeJsonResponse(w http.ResponseWriter, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
+	if err := json.NewEncoder(w).Encode(rd); err != nil {
+		http.Error(w, "Failed To Encode Error", http.StatusInternalServerError)
+	}
 }
 
 func imAlive(w http.ResponseWriter, r *http.Request) {
-
+	var returnData Response = Response{
+		"success", "Ping was successfull", Data{},
+	}
+	returnData.writeJsonResponse(w, 200)
 }
 
 func StartServer() {
 	http.HandleFunc("/health", imAlive)
+	log.Println("Server running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
