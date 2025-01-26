6eba84aa1690bb2788a3552a3cf633a4ed5e59ef
Daniel Shogbon <ishogbon@gmail.com>
2025-01-26 19:03:50 +0100

    added read messages

--- server/websocket/client.go
+++ server/websocket/client.go
@@ -1,6 +1,10 @@
 package websocket
 
-import "github.com/gorilla/websocket"
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
 
 type ClientList map[*Client]bool
 
@@ -13,5 +17,23 @@ func NewClient(connection *websocket.Conn, manager *Manager) *Client {
 	return &Client{
 		connection,
 		manager,
+	}
+}
+
+func (client *Client) readMessages() {
+	defer func() {
+
+	}()
+
+	for {
+		messageType, payload, err := client.connection.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("Unexpected Close Error %v", err)
+			}
+			break
+		}
+		log.Print(messageType)
+		log.Print(payload)
 	}
 }
