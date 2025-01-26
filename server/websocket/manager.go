aa809793dee31f27dae57b69de7c094b9ae109f1
Daniel Shogbon <ishogbon@gmail.com>
2025-01-26 18:48:32 +0100

    added add and remove clients

--- server/websocket/manager.go
+++ server/websocket/manager.go
@@ -10,3 +10,20 @@ type Manager struct {
 func NewManager() *Manager {
 	return &Manager{}
 }
+
+func (manager *Manager) addClient(client *Client) {
+	manager.Lock()
+	defer manager.Unlock()
+
+	manager.clients[client] = true
+}
+
+func (manager *Manager) removeClient(client *Client) {
+	manager.Lock()
+	defer manager.Lock()
+
+	if _, ok := manager.clients[client]; ok {
+		client.connection.Close()
+		delete(manager.clients, client)
+	}
+}
